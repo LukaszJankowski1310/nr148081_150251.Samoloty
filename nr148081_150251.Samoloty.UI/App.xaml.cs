b375212c9a0f6e3087bdf464e0f602ace37c3f48
Łukasz Jankowski <lukasz.jankowski1310@gmail.com>
2024-01-27 01:08:10 +0100

    wpf desktop init

---
+++ nr148081_150251.Samoloty.UI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace nr148081_150251.Samoloty.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ nr148081_150251.Samoloty.UI/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace nr148081_150251.Samoloty.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
